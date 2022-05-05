e596bce72bcfc06748336abaf349a18d8b54e4e0
Bilal Ahmad <1704922@brunel.ac.uk>
2022-05-05 16:26:30 +0100

    Updated functionality for Discount Calculator: - Hashmaps created as attributes to hold item metadata. - New function created to traverse the basket and index the item metadata in the Hashmaps.

--- src/main/java/kata/supermarket/DiscountCalculator.java
+++ src/main/java/kata/supermarket/DiscountCalculator.java
@@ -1,12 +1,32 @@
 package kata.supermarket;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 
 public class DiscountCalculator {
 
-    public DiscountCalculator(List<Item> basket){
+    /***
+     * Used hashmaps here to avoid repeated exhaustive search and reduce time complexity.
+     * The sizes of these maps will be the same so space complexity isnt increased
+     * this could be refactored but given time constraints I will stick with this.
+     * the three hashmaps will store the following:
+     * itemCountMap -> key: the productID (e.g. 101) Value: how many are in the basket (e.g.30)
+     * itemDiscountMap -> key: the productID (e.g. 101) Value: The product Dicount Code (e.g.1)
+     * itemPriceMap -> key: the productID (e.g. 101) Value: price of the product (e.g.0.49)
+     */
+    private final HashMap<Integer, Integer> itemCountMap;
+    private final HashMap<Integer, Integer> itemDiscountMap;
+    private final HashMap<Integer, BigDecimal> itemPriceMap;
+    private final List<Item> basket;
 
+    public DiscountCalculator(List<Item> basket){
+        //initialises all attributes of class then indexes item Metadata for the maps
+        this.basket = basket;
+        this.itemCountMap = new HashMap<Integer, Integer>();
+        this.itemDiscountMap = new HashMap<Integer, Integer>();
+        this.itemPriceMap = new HashMap<Integer, BigDecimal>();
+        this.indexItemMetadataInMap();
     }
 
 
@@ -17,5 +37,34 @@ public class DiscountCalculator {
 
     public BigDecimal calculateBuyOneGetOneFreeDiscount(BigDecimal price, int count) {
         return new BigDecimal("0");
+    }
+
+    public void indexItemMetadataInMap(){ // This function will take all the items in the basket and map the correct metadata to the maps and counts the amount of each item
+        for(Item item: getBasket()){ // loops through all items in bucket
+            if(getItemCountMap().get(item.getProductId()) == null && getItemDiscountMap().get(item.getDiscountCode()) == null){ // checks if product is not already in maps
+                getItemCountMap().put(item.getProductId(), 1); //assigns initial count to itemCountmap
+                getItemDiscountMap().put(item.getProductId(), item.getDiscountCode()); // assigns price to itemDiscountMap for the product ID
+                getItemPriceMap().put(item.getProductId(), item.getPrice()); //assigns price to itemPriceMap for the product ID
+            }
+            else{
+                getItemCountMap().put(item.getProductId(), getItemCountMap().get(item.getProductId()) + 1); //increases count for the product Id in the itemCountMap
+            }
+        }
+    }
+
+    public HashMap<Integer,Integer> getItemDiscountMap(){
+        return this.itemDiscountMap;
+    }
+
+    public List<Item> getBasket() {
+        return basket;
+    }
+
+    public HashMap<Integer, Integer> getItemCountMap() {
+        return itemCountMap;
+    }
+
+    public HashMap<Integer, BigDecimal> getItemPriceMap() {
+        return itemPriceMap;
     }
 }
